21319ad0247fdb2e56a0bfa6e07111fcf20f9e8f
hadryan <hadryan2001@gmail.com>
2023-11-29 16:48:32 -0300

    [VIDEO #11] - add lombok

--- src/main/java/br/com/hadryan/controller/AnimeController.java
+++ src/main/java/br/com/hadryan/controller/AnimeController.java
@@ -1,12 +1,14 @@
 package br.com.hadryan.controller;
 
 import br.com.hadryan.domain.Anime;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping({"v1/animes", "v1/animes/"})
+@Log4j2
 public class AnimeController {
 
     @GetMapping
@@ -17,6 +19,7 @@ public class AnimeController {
 
     @GetMapping("filter")
     public List<Anime> filter(@RequestParam(name = "name") String name) {
+        log.info("Request recevied to list all animes, params '{}'", name);
         var animes = Anime.getAnimes();
         if (name == null) return animes;
         return animes
@@ -27,6 +30,7 @@ public class AnimeController {
 
     @GetMapping("{id}")
     public Anime findByName(@PathVariable Long id) {
+        log.info("Request recevied to find animes by id '{}'", id);
         return Anime.getAnimes().stream()
                 .filter(n -> n.getId().equals(id))
                 .findFirst()
--- src/main/java/br/com/hadryan/domain/Anime.java
+++ src/main/java/br/com/hadryan/domain/Anime.java
@@ -1,44 +1,26 @@
 package br.com.hadryan.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Anime {
 
     private Long id;
     private String name;
 
-    public Anime() {
-    }
-
-    public Anime(long id, String name) {
-        this.id = id;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     @JsonIgnore
     public static List<Anime> getAnimes() {
         return List.of(
-                new Anime(1, "Naruto"),
-                new Anime(2, "Bleach"),
-                new Anime(3, "Dragonball Z"));
+                new Anime(1L, "Naruto"),
+                new Anime(2L, "Bleach"),
+                new Anime(3L, "Dragonball Z"));
     }
 
 }
