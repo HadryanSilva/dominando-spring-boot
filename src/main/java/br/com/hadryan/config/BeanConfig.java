450904dcec648a8b3c2df122608ebdb3e976d8c1
HadryanSilva <hadryan2001@gmail.com>
2024-01-02 18:52:01 -0300

    [VIDEO #46] - Properties, YML, Profiles

--- src/main/java/br/com/hadryan/config/BeanConfig.java
+++ src/main/java/br/com/hadryan/config/BeanConfig.java
@@ -1,5 +1,6 @@
 package br.com.hadryan.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import test.outside.Connection;
@@ -7,9 +8,23 @@ import test.outside.Connection;
 @Configuration
 public class BeanConfig {
 
+    @Value("${database.url}")
+    private String urlMySql;
+
+    @Value("${database.username}")
+    private String usernameMySql;
+
+    @Value("${database.password}")
+    private String passwordMySql;
+
     @Bean
     public Connection connection() {
-        return new Connection("localhost", "user", "xxx");
+        return new Connection(urlMySql, usernameMySql, passwordMySql);
+    }
+
+    @Bean(name = "mongoDB")
+    public Connection connectionMongoDb() {
+        return new Connection("localhost", "mongodb", "xxxx");
     }
 
 }
